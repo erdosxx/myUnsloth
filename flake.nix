8dfa5979b9f6976563f69b87bc78334ac655159f
Norel <norel.evoagile@gmail.com>
2024-09-09 09:13:19 +0900

    Resolve install issue for unsloth and xformers.

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    };
     # nixpkgs.url = "nixpkgs";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    /* nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; */
     # nixpkgs.url = "github:NixOS/nixpkgs?&rev=632d9a851e1db6736135f73df4dae76469a72168";
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     poetry2nix = {
@@ -21,25 +23,42 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
+
+          /* overlays = [ xformers ]; */
+        };
+
         inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; })
           mkPoetryApplication defaultPoetryOverrides;
 
         # Ex: pypkgs-build-requirements = { textgrad = [ "setuptools" ]; };
-        pypkgs-build-requirements = { };
+        pypkgs-build-requirements = {
+          xformers = [ "setuptools" ];
+          unsloth = [ "setuptools" ];
+        };
 
         p2n-overrides = defaultPoetryOverrides.extend (final: prev:
-          builtins.mapAttrs (package: build-requirements:
-            (builtins.getAttr package prev).overridePythonAttrs (old: {
-              buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg:
-                if builtins.isString pkg then
-                  builtins.getAttr pkg prev
-                else
-                  pkg) build-requirements);
-            })) pypkgs-build-requirements);
+          builtins.mapAttrs
+            (package: build-requirements:
+              (builtins.getAttr package prev).overridePythonAttrs (old: {
+                buildInputs = (old.buildInputs or [ ]) ++ (builtins.map
+                  (pkg:
+                    if builtins.isString pkg then
+                      builtins.getAttr pkg prev
+                    else
+                      pkg)
+                  build-requirements);
+              }))
+            pypkgs-build-requirements);
         python_ver = "python312";
         pythonPackages = pkgs.${python_ver + "Packages"};
-      in {
+      in
+      {
         packages = {
           myapp = mkPoetryApplication {
             projectDir = self;
@@ -50,6 +69,7 @@
           };
           default = self.packages.${system}.myapp;
         };
+
 
         # Shell for app dependencies.
         #
@@ -65,14 +85,21 @@
         #
         # Use this shell for changes to pyproject.toml and poetry.lock.
         devShells.poetry = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.myapp ];
+          inputsFrom = [
+            self.packages.${system}.myapp
+          ];
+
           packages = with pkgs; [
-            # python311
             # (poetry.override { python3 = python311; })
             poetry
             # for qtconsole
             pythonPackages.pyside2
-            # python312Packages.setuptools
+            /* pythonPackages.xformers */
+            /* pythonPackages.torch */
+            /* pythonPackages.torchvision */
+            /* pythonPackages.torch */
+            /* cudatoolkit */
+            linuxPackages.nvidia_x11
             qt5Full
             gnugrep
             gawk
@@ -80,6 +107,8 @@
           ];
           shellHook = ''
             alias vi="nvim"
+            export CUDA_PATH=${pkgs.cudatoolkit}
+            export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
             just qt
           '';
         };
